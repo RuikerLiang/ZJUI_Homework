bbaa0ae6137eaa51807019a9b3261031293f2c01
kerui.23@intl.zju.edu.cn <kerui.23@intl.zju.edu.cn>
2024-02-26 16:06:33 +0800

    5_9

--- MATH/CAL_II_5_9.py
+++ MATH/CAL_II_5_9.py
@@ -1,22 +1,42 @@
-# This program is to solve the 9th problem in CAL_II_5 homework
-
 from math import pi, sin
 
-n = 10
+from matplotlib.ticker import EngFormatter
+
+n = 40
 ans = 0
+end_ans = 26
 delta = pi / n
+exp = 0.00001
+# M
 for i in range(0, n):
 	xi = (delta * i + delta * (i + 1)) / 2
 	ans = ans + sin(xi) * delta
-print(ans * 13)
+ans = ans * 13
+print(ans)
+print((end_ans - ans))
+E_m = 13* pi ** 3 / (24 * n * n)
+print(E_m)
+i = 1
+while (13 * pi ** 3 / (24 * i * i) > exp):
+	i = i + 1
+print(i)
 
+# T
 ans = 0
 for i in range(0, n):
 	yi = (sin(delta * i) + sin(delta * (i + 1))) / 2
 	ans = ans + yi * delta
 ans = ans * 13
 print(ans)
+print(end_ans - ans)
+E_t = 13 * pi ** 3 / (12 * n * n)
+print(E_t)
+i = 1
+while (13 * pi ** 3 / (12 * i * i) > exp):
+	i = i + 1
+print(i)
 
+# S
 ans = 0
 for i in range(0, n + 1):
 	if (i == 0 or i == n):
@@ -29,3 +49,6 @@ for i in range(0, n + 1):
 	ans = ans + sin(i * delta) * xi * delta
 ans = ans * 13 / 3
 print(ans)
+print(end_ans - ans)
+E_s = 13 * pi ** 5 / (180 * n ** 4)
+print(E_s)
